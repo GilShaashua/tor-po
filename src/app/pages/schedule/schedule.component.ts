b4709e75ab31fdff891f0cf309ed7877d973dc77
Gil Shaashua <gilshaashua@gmail.com>
2024-07-21 17:36:54 +0300

    appointments improvements

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -41,6 +41,7 @@ export class ScheduleComponent implements OnInit {
         this.scheduleService.getAppointments().subscribe({
             next: (appointments) => {
                 this.appointments = appointments
+                this._getEmptyAppointments()
             },
             error: (err) => {
                 console.error('err', err)
@@ -59,25 +60,76 @@ export class ScheduleComponent implements OnInit {
         })
     }
 
-    calculateAppointmentStyle(appointment: Appointment) {
-        const scheduleStartTime = 8 // Assuming your schedule starts at 8 AM
-        const hourHeight = 60 + 20 // Assuming each hour in the schedule is represented by 60px
+    private _getEmptyAppointments() {
+        const emptyAppointments: Appointment[] = []
+        let lastEndTime = '08:00' // Start of the day
+
+        this.appointments.sort((a, b) => a.timeStart.localeCompare(b.timeStart)) // Ensure appointments are sorted
+
+        for (const appointment of this.appointments) {
+            if (appointment.timeStart > lastEndTime) {
+                // There's a gap
+                emptyAppointments.push({
+                    id: -1, // -1 or any other placeholder to indicate an empty slot
+                    title: '',
+                    clientName: '',
+                    timeStart: lastEndTime,
+                    timeEnd: appointment.timeStart,
+                })
+            }
+            lastEndTime = appointment.timeEnd
+        }
+
+        // Check for a gap between the last appointment and the end of the day
+        if (lastEndTime < '18:00') {
+            emptyAppointments.push({
+                id: -1,
+                title: '',
+                clientName: '',
+                timeStart: lastEndTime,
+                timeEnd: '18:00',
+            })
+        }
+
+        // Merge the booked and empty appointments
+        this.appointments = [...emptyAppointments, ...this.appointments].sort(
+            (a, b) => a.timeStart.localeCompare(b.timeStart)
+        )
+    }
+
+    getAppointmentStyle(
+        appointment: Appointment,
+        isEmpty: boolean,
+        isLatest: boolean
+    ) {
+        const scheduleStartTime = 8 // schedule starts at 8 AM
+        const hourHeight = 60 + 35 // each hour in the schedule is represented by 60px
 
         const startTime = this._extractHour(appointment.timeStart)
         const endTime = this._extractHour(appointment.timeEnd)
 
         let top = (startTime - scheduleStartTime) * hourHeight
-        let height = (endTime - startTime) * hourHeight + 20
+        let height = (endTime - startTime) * hourHeight
 
-        if (this._isDecimal(startTime) && this._isDecimal(endTime)) {
-            top += 5
-            height -= 12
-        }
-
-        return {
-            top: `${top}px`,
-            height: `${height}px`,
-            position: 'absolute',
+        if (isLatest) {
+            return {
+                top: `${top + 3}px`,
+                height: `${height - 30}px`,
+                position: 'absolute',
+            }
+        } else if (isEmpty) {
+            return {
+                top: `${top + 3}px`,
+                height: `${height - 10}px`,
+                position: 'absolute',
+            }
+        } else {
+            return {
+                top: `${top}px`,
+                height: `${height - 5}px`,
+                position: 'absolute',
+                'z-index': 999,
+            }
         }
     }
 
@@ -89,9 +141,5 @@ export class ScheduleComponent implements OnInit {
         }
 
         return hour
-    }
-
-    private _isDecimal(num: number) {
-        return num % 1 !== 0
     }
 }
